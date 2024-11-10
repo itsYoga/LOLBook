aebf93592a9819d17425e4560530985133edcc59
YuJiaLiang0702 <ch993115@gmail.com>
2024-11-10 08:19:28 +0800

    Initial Commit

---
+++ LOLBook/LOLBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOLBookApp.swift
+//  LOLBook
+//
+//  Created by Jesse Liang on 2024/11/10.
+//
+
+import SwiftUI
+
+@main
+struct LOLBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LOLBookTests/LOLBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  LOLBookTests.swift
+//  LOLBookTests
+//
+//  Created by Jesse Liang on 2024/11/10.
+//
+
+import Testing
+@testable import LOLBook
+
+struct LOLBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
